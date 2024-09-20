d22f21b860433c735c11ff96c6404f98516986a3
achira7 <achira.sanjana@gmail.com>
2024-09-20 23:11:15 +0530

    created the Template forr the Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-center'>
+        <h1 className='text-color-secondary text-xl'>Designed and Developed by</h1>
+        <h1 className='text-color-primary text-2xl'>Achira Silva</h1>
+    </div>
+  )
+}
+
+export default Footer
